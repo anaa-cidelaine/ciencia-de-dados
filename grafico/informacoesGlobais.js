caca2d786e9a6946d8d0d60190f0915606f98690
anaa-cidelaine <ana.cidelainee@gmail.com>
2024-10-03 07:39:21 -0300

    ok

---
+++ grafico/informacoesGlobais.js
@@ -0,0 +1,14 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+        const res = await fetch('https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json')
+
+        const dados = await res.json()
+
+        console.log(dados);
+
+    }
+
+    visualizarInformacoesGlobais()
+ 
