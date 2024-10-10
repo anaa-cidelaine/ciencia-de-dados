3e6f1c909d6f5d11cc4a8b0d2075a4ee97fb3fb7
anaa-cidelaine <ana.cidelainee@gmail.com>
2024-10-10 07:40:28 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,13 +2,14 @@ const (url) = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados
 
 async function visualizarInformacoesGlobais() {
 
-        const res = await fetch()
+    const res = await fetch()
 
-        const dados = await res.json()
+    const dados = await res.json()
 
-        console.log(dados);
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem ${ dados.total_pessoas_mundo } de pessoas e que aproximadamente ${ dados.total_pessoas_conectadas } estão conectadas em alguma rede social e passam em média ${ dados.tempo_medio } horas conectadas.'
+        console.log(paragrafo)
+}
 
-    }
-
-    visualizarInformacoesGlobais()
- 
+visualizarInformacoesGlobais()
